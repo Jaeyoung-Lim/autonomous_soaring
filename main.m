70ace6c2f782b5bedfeb1ca62088856b8926b055
JaeyoungLim <gokoreas@gmail.com>
2018-12-28 19:35:44 +0100

    wip Implement wind gradients

--- main.m
+++ main.m
@@ -6,35 +6,37 @@ clc; clear all;
 
 
 state = struct('gamma_a', 0, ...
-               'psi_a', 0, ...
+               'psi_a', 0.1, ...
                'phi', 0, ...
                'pos', [0.0, 0.0, -1.0], ...
                'vel', [10.0, 0.0, 0.0], ...
-               'v_a', 10.0); 
+               'v_a', 10.0, ...
+               'W', [0.0, 0.0, 0.0]);
 
-roll_rate = 0.5;
+roll_rate = -0.0;
 pitch_rate = 0.5;
 
 Jw = [0.0, 0.0, 0.0;
       0.0, 0.0, 0.0;
       0.0, 0.0, 0.0];
-  
-wind_gradient = Jw;
 pos = [];
 loop = 0;
+
 while true
-    state = step(state, roll_rate, pitch_rate, wind_gradient);
+    Jw = getwindJacobian(state);
+    state = step(state, roll_rate, pitch_rate, Jw);
     
     [~, ~, ~, pos1] = getstate(state);
     pos =[pos; pos1];
     figure(1);
     
-    plot3(pos(:, 1), pos(:, 2), -pos(:, 3), 'r-');hold on; axis equal; grid on; zlim([-1, 3]);
+    plot3(pos(:, 1), pos(:, 2), -pos(:, 3), 'r-');hold on; axis equal; grid on; %zlim([-1, 3]);
     xlabel('x Position [m]'); ylabel('y Position [m]'); zlabel('z Position [m]');
 %     if loop - 60 >= 0 || loop == 0
 %         plotaircraft(state); hold on;
 %         loop = 0;
 %     end
+%      plotwindfield(wind_gradient);
      plotaircraft(state); hold off;
 
     if islanded(state)
@@ -43,3 +45,11 @@ while true
     loop = loop + 1;
 end
 
+function Jw = getwindJacobian(state)
+    pos = state.pos;
+    
+    Jw = [0.0, 0.0, 0.0;
+          0.0, 0.0, 3.0;
+          0.0, 0.0, 0.0];
+
+end
